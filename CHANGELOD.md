1060941375b1fd8a54f536aecda8aeb45144b15f
Reinaldo Romero <romerorei21@gmail.com>
2023-04-19 23:06:33 -0400

    Add config additional rbc

--- CHANGELOD.md
+++ CHANGELOD.md
@@ -2,3 +2,4 @@
 - Se agregan las rutas de la aplicacion
 - LoginPage y NavBar
 - React Big Calendar
+- Configuracion adicionales al calendario
--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,28 +1,17 @@
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import { Calendar } from 'react-big-calendar'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 
-import { addHours, format, parse, startOfWeek, getDay } from 'date-fns';
-import enUS from 'date-fns/locale/en-US'
-
+import { addHours } from 'date-fns';
 import { Navbar } from "../"
 
-const locales = {
-  'en-US': enUS,
-}
+import { getMessagesES, localizer } from '../../helpers';
 
-const localizer = dateFnsLocalizer({
-  format,
-  parse,
-  startOfWeek,
-  getDay,
-  locales,
-})
 
 const events = [{
   title: 'Cumpleaños del perro',
   notes: 'Comprar torta',
   start: new Date(),
-  end: addHours( new Date(), 2 ),
+  end: addHours( new Date(), 0 ),
   bgColor: '#fafafa',
   user: {
     _id: '123',
@@ -32,16 +21,34 @@ const events = [{
 }]
 
 export const CalendarPage = () => {
+
+  const eventStyleGetter = ( event, start, end, isSelected ) => {
+    console.log({ event, start, end, isSelected });
+
+    const style = {
+      backgroudColor: '#347CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white'
+    }
+
+    if (isSelected ) return { style };
+
+  }
+
   return (
     <>
       <Navbar />
 
       <Calendar
+        culture='es'
         localizer={ localizer }
         events={ events }
         startAccessor="start"
         endAccessor="end"
         style={{ height: 'calc( 100vh - 80px )' }}
+        messages={ getMessagesES() }
+        eventPropGetter={ eventStyleGetter }
       />
     </>
   )
