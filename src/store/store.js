5f9cfb14f8a47822e115b401c9856f36037f1330
Reinaldo Romero <romerorei21@gmail.com>
2023-04-23 02:23:32 -0400

    fix warning about date serializer

--- src/store/store.js
+++ src/store/store.js
@@ -5,6 +5,9 @@ export const store = configureStore({
   reducer: {
     calendar: calendarSlice.reducer,
     ui: uiSlice.reducer
-  }
+  },
+  middleware: ( getDefaultMiddleware ) => getDefaultMiddleware({
+    serializableCheck: false // esto es para que js no revise si puede serializar las fechas
+  })
 
 })
